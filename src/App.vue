26ebd71bb1914e25c8b0e7c49d805314b23494bd
hotdogc1017 <hotdogc1017@gmail.com>
2025-08-21 21:58:18 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Button, Alert } from "heroui-vue";
+</script>
+
+<template>
+    <div class="flex h-svh w-full justify-center items-center">
+        <div class="flex flex-col gap-4">
+            <Alert
+                color="success"
+                title="Hello, world!"
+                description="Power by Vue and HeroUI Theme"
+            ></Alert>
+            <Button color="primary">Click me</Button>
+        </div>
+    </div>
+</template>
---
+++ src/heroui.ts
@@ -0,0 +1,4 @@
+// heroui.ts
+import { heroui } from "@heroui/theme";
+
+export default heroui(); // 别忘记调用`heroui`函数
